fe4bff7587ddfb974a8fd540c08d8e86f9ec8168
Daniel González <daniel.gonzalez@freelancemadrid.es>
2013-02-27 18:37:23 +0100

    initial import

---
+++ Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Desarrolla2\Bundle\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+
+    /**
+     * @Route("/motivacion")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+
+}
